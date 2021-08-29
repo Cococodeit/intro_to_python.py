0f7f33309985231842f9f1e622686cce448ca977
cococodeit <67393862+Cococodeit@users.noreply.github.com>
2021-08-29 22:54:56 +0000

    Added variables and functions

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+calculations_to_seconds = 24 * 60 * 60
+name_of_unit = "seconds"
+
+def days_to_units(): 
+    print(f"20 days are {20 * calculations_to_seconds} {name_of_unit}")
+    print("THIS IS AN EXAMPLE OF A FUNCTION")
+
+days_to_units()
+
+
+
+
+
+print(f"20 days are {20 * calculations_to_seconds} {name_of_unit}")
+print(f"35 days are {35* calculations_to_seconds} {name_of_unit}")
+print(f"50 days are {50 * calculations_to_seconds} {name_of_unit}")
+print(f"110 days are {110 * calculations_to_seconds} {name_of_unit}")
