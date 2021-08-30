a91b36271edcb82dc326bd1d721cc20ee57f1629
cococodeit <67393862+Cococodeit@users.noreply.github.com>
2021-08-30 00:32:57 +0000

    changed number of days in the function to 1000

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ calculations_to_seconds = 24 * 60 * 60
 name_of_unit = "seconds"
 
 def days_to_units(): 
-    print(f"20 days are {20 * calculations_to_seconds} {name_of_unit}")
+    print(f"1000 days are {1000 * calculations_to_seconds} {name_of_unit}")
     print("THIS IS AN EXAMPLE OF A FUNCTION")
 
 days_to_units()
